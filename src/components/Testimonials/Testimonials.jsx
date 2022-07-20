85ff508d170d9642799292cd813012c2e7f10b05
Graciela Romano <graci.romano@gmail.com>
2022-07-20 17:40:11 -0300

    Traduccion de componente Testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -5,30 +5,32 @@ import { useState } from 'react'
 import leftArrow from "../../assets/leftArrow.png"
 import rightArrow from "../../assets/rightArrow.png"
 import {motion} from 'framer-motion'
+import { useTranslation } from 'react-i18next'
 
 const Testimonials = () => {
     const transition = {type: "spring", duration:3}
     const [selected, setSelected] = useState(0);
     const tLength = testimonialsData.length;
+    const [t, i18n] = useTranslation("global")
   return (
     <div className="testimonials">
         <div className="left-t">
-            <span>Testimonials</span>
-            <span className='stroke-text'>What they</span>
-            <span>say about us</span>
+            <span>{t("testimonials.title_1")}</span>
+            <span className='stroke-text'>{t("testimonials.title_2")}</span>
+            <span>{t("testimonials.title_3")}</span>
             <motion.span
                 key={selected}
                 initial={{ opacity: 0, x: -100}}
                 animate={{opacity:1, x: 0}}
                 exit={{opacity:0, x: 100}}
                 transition={transition}>
-                {testimonialsData[selected].review}
+                {t(testimonialsData[selected].review)}
             </motion.span>
             <span>
                 <span style={{color: 'var(--orange)'}}>
-                    {testimonialsData[selected].name}
+                    {t(testimonialsData[selected].name)}
                 </span>{" "}
-                 - {testimonialsData[selected].status}
+                 - {t(testimonialsData[selected].status)}
             </span>
         </div>
         <div className="right-t">
--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -5,21 +5,20 @@ import image3 from "../assets/t-image3.jpg";
 export const testimonialsData = [
   {
     image: image1,
-    review:
-      "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
-    status : 'ENTREPRENEUR'
+    review:"testimonials.review_1",
+    name: "testimonials.name_1",
+    status : "testimonials.status_1"
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
-    name: 'JOHN KEVIN',
-    status: 'COACH'
+    review: "testimonials.review_2",
+    name: "testimonials.name_2",
+    status: "testimonials.status_2"
   },
   {
     image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
-    name: 'FRANKLIN',
-    status: "CUSTOMER"
+    review:"testimonials.review_3",
+    name: "testimonials.name_3",
+    status: "testimonials.status_3"
   }
 ];
